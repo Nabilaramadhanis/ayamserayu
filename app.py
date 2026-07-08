c648ddca49029ddbfb0234f5c30ce8eee29f381d
Nabilaramadhanis <202210715258@mhs.ubharajaya.ac.id>
2026-07-08 21:47:53 +0700

    Ready for Render

--- app.py
+++ app.py
@@ -736,4 +736,5 @@ def export_stok_besok():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
